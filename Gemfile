671f05caec898a06efc7e78058e735fdf8d2bafa
youngcitrus <mrjustinnguyen@gmail.com>
2019-11-05 00:29:33 -0800

    built backend functionality for creating user

--- Gemfile
+++ Gemfile
@@ -52,6 +52,7 @@ group :development do
   gem 'better_errors'
   gem 'binding_of_caller'
   gem 'annotate'
+  gem 'jquery-rails'
 end
 
 group :test do
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,7 +2,7 @@ class Api::UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-          login!(@user)
+          login(@user)
           render :show
         else
           render json: @user.errors.full_messages, status: 422
@@ -11,7 +11,7 @@ class Api::UsersController < ApplicationController
 
     private
     def user_params
-        params.require(:user).permit(:username, :password)
+        params.require(:user).permit(:username, :name, :email, :password)
     end
 
 end
