c2cba4f9de9c187c73a9630eb261a712209d18e7
youngcitrus <mrjustinnguyen@gmail.com>
2020-02-07 22:44:13 -0800

    cleaning syntax

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Footer from '../footer';
 import NavBarContainer from '../navbar_container';
