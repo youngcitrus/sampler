9c33ae10462517b9df26eb87ca528eec2656cd1d
youngcitrus <mrjustinnguyen@gmail.com>
2020-03-04 23:31:13 -0800

    cleaning syntax

--- frontend/components/likes/pack_like.jsx
+++ frontend/components/likes/pack_like.jsx
@@ -4,7 +4,6 @@ import * as PackLikeUtil from "../../util/pack_like_util";
 class PackLike extends React.Component{
     constructor(props){
         super(props);
-        // debugger;
         this.packLike = { user_id: props.userId, pack_id: props.pack.id };
         this.state = {liked: props.pack.liked};
         this.handleClick = this.handleClick.bind(this);
