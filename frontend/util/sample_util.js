b845fbe821d28cbdf122bbfee40dff3b7b493a64
youngcitrus <mrjustinnguyen@gmail.com>
2020-03-22 23:42:14 -0700

    syntax cleanup

--- frontend/util/sample_util.js
+++ frontend/util/sample_util.js
@@ -10,18 +10,18 @@ export const fetchSamplePack = packId => {
         method: 'GET',
         url: `api/sample_packs/${packId}`
     });
-}
+};
 
 export const fetchSamples = packId => {
     return $.ajax({
         method: 'GET',
         url: `api/sample_packs/${packId}/samples`
     })
-}
+};
 
 export const fetchLikedSamples = () => {
     return $.ajax({
         method: 'GET',
         url: 'api/liked_samples'
     });
-}
+};
