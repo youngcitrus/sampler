1fbda772d18807d7d5104d5d014f2e1bb9190208
youngcitrus <mrjustinnguyen@gmail.com>
2020-04-07 23:37:37 -0700

    add liked samples pack show page

--- frontend/components/liked_samples.jsx
+++ frontend/components/liked_samples.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Sample from './samples/sample';
+import NavBarShowContainer from './navbar_show_container';
 
 class LikedSamples extends React.Component{
     constructor(props) {
@@ -25,24 +26,37 @@ class LikedSamples extends React.Component{
         } else {
             return (
                 <div className="liked-samples-container">
-                    <div className='sample-headers'>
-                        <div className='sh-pack-filename'>
-                            <div className='sh-pack'>Pack</div>
-                            <div className='sh-filename'>Filename</div>
+                    <NavBarShowContainer redirect={this.props.redirectToRoot}/>
+                    <div className='pack-show'>
+                        <div className='pack-banner'>
+                            <img src="https://sampler-dev.s3-us-west-1.amazonaws.com/seeds/cover-art/favorites-cover.png" className='cover-art'/>
+                            <img src="https://sampler-dev.s3-us-west-1.amazonaws.com/seeds/cover-art/favorites-cover.png" className='cover-art-background'/>
+                            <div className='pack-info'>
+                                <h2>My Favorites</h2>
+                                <p>All of your favorite samples - in one place.</p>
+                            </div>
+
+                            <div className='darken'></div>
                         </div>
-                        <div className='sh-info'>
-                            <div className='sh-key'>Key</div>
-                            <div className='sh-bpm'>BPM</div>
-                            <div className='sh-like'>Like</div>
-                            <div className='sh-cart'>Download</div>
+                        <div className='sample-headers'>
+                            <div className='sh-pack-filename'>
+                                <div className='sh-pack'>Pack</div>
+                                <div className='sh-filename'>Filename</div>
+                            </div>
+                            <div className='sh-info'>
+                                <div className='sh-key'>Key</div>
+                                <div className='sh-bpm'>BPM</div>
+                                <div className='sh-like'>Like</div>
+                                <div className='sh-cart'>Download</div>
+                            </div>
                         </div>
-                    </div>
-                    <div className="liked-samples"> 
-                        { this.props.likedSamples.map( sample => {
+                        <div className="liked-samples"> 
+                            { this.props.likedSamples.map( sample => {
+                                
+                                return (<Sample key={sample.id} sample={sample} pack={this.props.samplePacks[sample.pack_id]} userId={this.props.userId} refresh={this.rerenderParentCallback} page="home"/>)
+                            }, this)}
                             
-                            return (<Sample key={sample.id} sample={sample} pack={this.props.samplePacks[sample.pack_id]} userId={this.props.userId} refresh={this.rerenderParentCallback} page="home"/>)
-                        }, this)}
-                        
+                        </div>
                     </div>
                 </div>
             )
